b03cf483a5f58596a3fcf50c6478b44cc0274fb5
Nathan Sjoquist <nathans@alleninstitute.org>
2017-07-11 12:23:22 -0700

    updating name

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-blue-sky',
-    version='0.11',
+    name='django-blue-sky-workflow-engine',
+    version='0.1',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
